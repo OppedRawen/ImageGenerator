e2f8fa158a51a862a0423ffba1e9871c1bcaa7ea
OppedRawen <yud2372@gmail.com>
2023-05-21 23:53:17 -0700

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
+import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
+import {logo} from './assets';
+import {Home,CreatePost} from './pages';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>Hello world</div>
+   <BrowserRouter>
+   <header className=' w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+    <Link to='/'>
+      <img src={logo} alt="logo" className='w-28 object-contain'></img>
+      <h1>Hellosasdasd</h1>
+    </Link>
+   </header>
+   </BrowserRouter>
   )
 }
 
