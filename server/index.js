e09ecd505657110841bfd82639464a74ca40e6c3
OppedRawen <yud2372@gmail.com>
2023-05-27 23:33:15 -0700

    connection to mongooDb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-
+// in nodejs, if we are importing from a file. we have to specify .js
+// in react we don't have to do that
+import connectDB from './mongodb/connect.js';
 dotenv.config();
 
 const app = express();
@@ -12,9 +14,16 @@ app.get('/', async(req,res)=>{
     res.send('Hello from Dalle API');
 });
 
+
 const startServer = async () => {
-    app.listen(8080,()=>{
-        console.log('Server is running on port 8080');
-    })};
+    try {
+        connectDB(process.env.MONGODB_URI);
+        app.listen(8080,()=>{
+            console.log('Server is running on port 8080');
+        })}catch (error) {
+            console.log(error);
+        }
+    } 
+   
 
 startServer();
