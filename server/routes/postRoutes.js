0d51bc69265185d02d9591fb3b81357d88daa3a7
OppedRawen <yud2372@gmail.com>
2023-05-28 23:52:59 -0700

    post route for share

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -5,7 +5,41 @@ import {v2 as cloudinary} from 'cloudinary';
 import Post from '../mongodb/models/post.js';
 // make sure the environment variable is populated
 dotenv.config();
-
 const router = express.Router();
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
 
+// get route to get all post
+router.route('/').get(async(req,res)=>{
+    try {
+        const posts = await Post.find({});
+        res.status(200).json({success:true,data:posts});
+    } catch (error) {
+        res.status(500).json({success:false,data:error});
+
+    }
+
+});
+// post route to create a post
+
+router.route('/').post(async(req,res)=>{
+
+try {
+    const {name,prompt,photo} = req.body;
+    //  upload image to cloudinary and return a photoURL
+    const photoURL = await cloudinary.uploader.upload(photo);
+    // using the photoURL to create a new post request with name, url, and prompt
+    const newPost = await Post.create({
+    name,
+    photo:photoURL.url,
+    prompt
+})
+res.status(201).json({success:true,data:newPost});
+} catch (error) {
+    res.status(500).json({success:false,error:error});
+}
+});
 export default router;
