b163fde814bb4559ecc13aff301e09afa65f3c60
OppedRawen <yud2372@gmail.com>
2023-05-28 23:13:47 -0700

    post route image

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,8 +19,28 @@ const CreatePost = ()=>{
     const handleSubmit = ()=>{
 
     }
-    const generateImg=()=>{
-    
+    const generateImg= async()=>{
+        if(form.prompt){
+            try{
+                setGeneratingImg(true);
+                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({prompt:form.prompt}),
+                });
+                const data= await response.json();
+                setForm({...form,photo:`image/jpeg;base64,${data.photo}`});
+            }catch(error){
+                alert(error);
+
+            }finally{
+                setGeneratingImg(false);
+            }
+        }else{
+            alert('Please enter a prompt');
+        }
     }
     const handleSurpriseMe = ()=>{
         const randomPrompt = getRandomPrompt(form.prompt);
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,3 +7,29 @@ import {Configuration,OpenAIApi} from 'openai';
 dotenv.config();
 
 const router = express.Router();
+
+const configugration = new Configuration({apiKey:process.env.OPENAI_API_KEY});
+// create an instance of open ai
+const openai = new OpenAIApi(configugration);
+
+router.route('/').get((req,res)=>{
+    res.send('Hello from Dalle');
+});
+router.route('/').post(async(req,res)=>{
+    try {
+        const {prompt}= req.body;
+        const aiResponse = await openai.createImage({
+            prompt:prompt,
+            n:1,
+            size:'1024x1024',
+            response_format:'b64_json'
+        });
+        const image = aiResponse.data.data[0].b64_json;
+        res.status(200).json({photo:image});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:error});
+    }
+})
+
+export default router;
