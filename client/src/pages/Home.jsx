93ff443a2637b82d0f7671ed33738ee74244ae52
OppedRawen <yud2372@gmail.com>
2023-05-29 18:34:13 -0700

    final product

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,7 +13,8 @@ const Home = ()=>{
     const [loading,setLoading]= useState(false);
     const [allPosts,setAllPosts]= useState(null);
     const[searchText,setSearchText]= useState('');
-
+    const [searchedResults,setSearchedResults]= useState(null);
+    const[searchTimeout,setSearchTimeout]= useState(null);
     const fetchPosts = async()=>{
         setLoading(true);
         try {
@@ -36,6 +37,19 @@ const Home = ()=>{
     useEffect(()=>{
        fetchPosts();
     },[]);
+
+    const handleSearchChange = (e)=>{
+        clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+    setTimeout(()=>{
+        const searchResults = allPosts.filter((item)=>item.name.toLowerCase().includes(searchText.toLowerCase())||item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+
+        setSearchedResults(searchResults);
+    },500)
+);
+    
+    }
     return(
         <section className='max-w-7x1 mx-auto'>
             <div>
@@ -44,7 +58,13 @@ const Home = ()=>{
             </div>
 
             <div className='mt-16'>
-                <FormField/>
+                <FormField labelName="Search posts" 
+                type="text"
+                name="text"
+                placeholder="Search posts"
+                value={searchText}
+                handleChange={handleSearchChange}  />
+
             </div>
 
             <div className='mt-10'>
@@ -58,7 +78,7 @@ const Home = ()=>{
                         </h2>
                     )}
                     <div className='grid lg:grid-cols-4 sm:grid-cols-3 x3:grid-cols-2 grid-cols-1 gap-3'>
-                        {searchText?(<RenderCards data={[]} title="No search results font" />):(<RenderCards data={allPosts} title="No posts found"/>)}
+                        {searchText?(<RenderCards data={searchedResults} title="No search results font" />):(<RenderCards data={allPosts} title="No posts found"/>)}
                     </div>
                     </>
                 )}
