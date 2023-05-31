1a9db2e3ae8545e600b0d46304816a442d21beaf
OppedRawen <yud2372@gmail.com>
2023-05-30 22:38:13 -0700

    asdasd

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = ()=>{
     const fetchPosts = async()=>{
         setLoading(true);
         try {
-            const response = await fetch('http://localhost:8080/api/v1/posts',{
+            const response = await fetch('https://imagegenerratee.herokuapp.com/api/v1/posts',{
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
