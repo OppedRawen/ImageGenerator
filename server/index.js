3701b20c0a74dc510197d04ef4818c3f0303cdc2
OppedRawen <yud2372@gmail.com>
2023-05-30 18:07:21 -0700

    ses

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(express.json({ limit: '50mb' }));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    const path = require('path');
+    
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
